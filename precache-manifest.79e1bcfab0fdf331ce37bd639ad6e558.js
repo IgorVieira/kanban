cdbe3fef51c55446845ee5440125abe033a3086c
Igor Vieira <igor.p.r.vieira@gmail.com>
2019-03-21 08:29:27 -0300

    Add files via upload

---
+++ precache-manifest.79e1bcfab0fdf331ce37bd639ad6e558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "056679683109e8e9276b",
+    "url": "/static/js/main.05667968.chunk.js"
+  },
+  {
+    "revision": "4eaeb2a2ff39c1c49dc9",
+    "url": "/static/js/1.4eaeb2a2.chunk.js"
+  },
+  {
+    "revision": "056679683109e8e9276b",
+    "url": "/static/css/main.d094814b.chunk.css"
+  },
+  {
+    "revision": "c1839183896718db015b691d04b15cf0",
+    "url": "/index.html"
+  }
+];
